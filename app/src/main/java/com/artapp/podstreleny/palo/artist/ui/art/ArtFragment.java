78ef1ffdb541bc332a7a32783f8194bfc07ca154
Palo Podstreleny <palo.podstreleny@gmail.com>
2018-08-24 23:38:58 +0800

    Updated main page, changed icon and created art fragment

---
+++ app/src/main/java/com/artapp/podstreleny/palo/artist/ui/art/ArtFragment.java
@@ -0,0 +1,22 @@
+package com.artapp.podstreleny.palo.artist.ui.art;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.artapp.podstreleny.palo.artist.R;
+
+public class ArtFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.art_fragment,container,false);
+        return view;
+    }
+}
--- app/src/main/java/com/artapp/podstreleny/palo/artist/ui/main/MainActivity.java
+++ app/src/main/java/com/artapp/podstreleny/palo/artist/ui/main/MainActivity.java
@@ -1,32 +1,23 @@
 package com.artapp.podstreleny.palo.artist.ui.main;
 
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.artapp.podstreleny.palo.artist.R;
-import com.artapp.podstreleny.palo.artist.network.Resource;
-import com.artapp.podstreleny.palo.artist.utils.ArtysToken;
-import com.artapp.podstreleny.palo.artist.utils.TokenUtil;
+import com.artapp.podstreleny.palo.artist.ui.art.ArtFragment;
 
 
-public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    private SharedPreferences mPreferences;
-    private TokenUtil mTokenUtil;
-    private String mToken;
+    private static final String ITEM_ID = "item_id";
+    private int selectedItem;
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -35,13 +26,12 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    FragmentManager manager = getSupportFragmentManager();
+                    manager.beginTransaction().replace(R.id.fragment_fl, new ArtFragment()).commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -53,71 +43,23 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPreferences = getSharedPreferences(getString(R.string.token_file_key), Context.MODE_PRIVATE);
-        mTokenUtil = new TokenUtil(this,mPreferences);
-
-        final MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        viewModel.getToken.observe(this, new Observer<Resource<ArtysToken>>() {
-            @Override
-            public void onChanged(@Nullable Resource<ArtysToken> token) {
-                if(token != null){
-                    switch (token.getStatus()){
-                        case ERROR:
-                            //Network problem
-                            Log.v(MainActivity.this.getLocalClassName(),"Error");
-                            break;
-                        case LOADING:
-                            //Loading
-                            Log.v(MainActivity.this.getLocalClassName(),"Loading");
-                            break;
-                        case SUCCESS:
-                            if(token.getData() != null && token.getData().getToken().length() > 0){
-                                mTokenUtil.saveToken(token.getData());
-                                Log.v(MainActivity.this.getLocalClassName(),"Saved token");
-                            }
-                            break;
-                        case UNAUTHORIZED:
-                            //Unauthorized
-                            Log.v(MainActivity.this.getLocalClassName(),"Unauthorized");
-                            break;
-
-                    }
-                }
-            }
-        });
-
-        if (!mTokenUtil.hasToken()){
-            viewModel.fetchToken();
-        }else {
-            mToken = mTokenUtil.getToken();
-            Toast.makeText(this,"Token: "+mToken,Toast.LENGTH_LONG).show();
-        }
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        Button mButton = findViewById(R.id.button);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        getSharedPreferences(getString(R.string.token_file_key),Context.MODE_PRIVATE).registerOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        getSharedPreferences(getString(R.string.token_file_key),Context.MODE_PRIVATE).unregisterOnSharedPreferenceChangeListener(this);
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        final String token = sharedPreferences.getString(key,null);
-        if(token != null){
-            mToken = token;
+        if (savedInstanceState != null && savedInstanceState.containsKey(ITEM_ID)) {
+            selectedItem = savedInstanceState.getInt(ITEM_ID);
+        } else {
+            selectedItem = R.id.navigation_home;
         }
+
+        navigation.setSelectedItemId(selectedItem);
+
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(ITEM_ID, selectedItem);
+        super.onSaveInstanceState(outState);
     }
 }
