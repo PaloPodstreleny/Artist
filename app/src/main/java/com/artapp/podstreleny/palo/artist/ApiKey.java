06b76cf0cecb56c29044d661f215821e22057d68
Palo Podstreleny <palo.podstreleny@gmail.com>
2018-08-30 05:05:45 +0800

    Ready to submit

--- app/src/main/java/com/artapp/podstreleny/palo/artist/ApiKey.java
+++ app/src/main/java/com/artapp/podstreleny/palo/artist/ApiKey.java
@@ -1,6 +1,7 @@
 package com.artapp.podstreleny.palo.artist;
 
 public class ApiKey {
+    //Provide CLIENT_ID AND CLIENT_SECRET
     public static final String CLIENT_ID = "";
     public static final String CLIENT_SECRET = "";
 }
